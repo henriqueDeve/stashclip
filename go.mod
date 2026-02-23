97530d8faee9ae1011d5c1dcaf0a7598d3283b2a
henriqueDeve <henriqueferrazgame@gmail.com>
2026-02-23 19:29:37 -0300

    feat: add X11 clipboard event watcher

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module stashclip
 
 go 1.21
+
+require github.com/BurntSushi/xgb v0.0.0-20220121173408-1a80f659f0b5
---
+++ internal/clipboard/x11_events.go
@@ -0,0 +1,93 @@
+package clipboard
+
+import (
+	"errors"
+
+	"github.com/BurntSushi/xgb"
+	"github.com/BurntSushi/xgb/xfixes"
+)
+
+// X11EventWatcher notifies when the X11 clipboard changes.
+type X11EventWatcher struct {
+	conn   *xgb.Conn
+	events chan struct{}
+	errs   chan error
+}
+
+// NewX11EventWatcher subscribes to X11 clipboard change events.
+func NewX11EventWatcher() (*X11EventWatcher, error) {
+	conn, err := xgb.NewConn()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := xfixes.Init(conn); err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	root := conn.Setup().DefaultScreen(conn).Root
+	atomCookie := xgb.InternAtom(conn, true, uint16(len("CLIPBOARD")), "CLIPBOARD")
+	atomReply, err := atomCookie.Reply()
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	mask := xfixes.SelectionEventMaskSetSelectionOwner |
+		xfixes.SelectionEventMaskSelectionWindowDestroy |
+		xfixes.SelectionEventMaskSelectionClientClose
+
+	xfixes.SelectSelectionInput(conn, root, atomReply.Atom, mask)
+
+	w := &X11EventWatcher{
+		conn:   conn,
+		events: make(chan struct{}, 1),
+		errs:   make(chan error, 1),
+	}
+
+	go w.loop()
+	return w, nil
+}
+
+// Events returns a channel that receives on clipboard changes.
+func (w *X11EventWatcher) Events() <-chan struct{} {
+	return w.events
+}
+
+// Errors returns a channel that receives async errors.
+func (w *X11EventWatcher) Errors() <-chan error {
+	return w.errs
+}
+
+// Close releases the X11 connection.
+func (w *X11EventWatcher) Close() error {
+	if w.conn == nil {
+		return nil
+	}
+	return w.conn.Close()
+}
+
+func (w *X11EventWatcher) loop() {
+	for {
+		event, err := w.conn.WaitForEvent()
+		if err != nil {
+			if !errors.Is(err, xgb.ErrInvalidConn) {
+				select {
+				case w.errs <- err:
+				default:
+				}
+			}
+			close(w.events)
+			return
+		}
+
+		switch event.(type) {
+		case xfixes.SelectionNotifyEvent:
+			select {
+			case w.events <- struct{}{}:
+			default:
+			}
+		}
+	}
+}
